115b40ad4890785a88a8783543526451c8ba2eab
Kasper Ramström <kasper.ram@gmail.com>
2018-11-06 17:13:11 +0100

    styling of sidebar & separated into multiple components #18

--- frontend/src/vue/src/components/App.vue
+++ frontend/src/vue/src/components/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <sidebar v-bind:newsList="newsList" ></sidebar>
+    <sidebar
+      v-bind:newsList="newsList"
+      v-bind:toggleActive="toggleActive"
+    ></sidebar>
   </div>
 </template>
 
@@ -12,16 +15,19 @@ export default {
   data () {
     return {
       newsList: [
-        { id: 0, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
-        { id: 1, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
-        { id: 2, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
-        { id: 3, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
+        { id: 0, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing", active: false},
+        { id: 1, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing", active: false},
+        { id: 2, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing", active: false},
+        { id: 3, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing", active: false},
       ]
     }
   },
   computed: {
   },
   methods: {
+    toggleActive: function(news) {
+      news.active = !news.active
+    }
   },
   components: {
     'sidebar': SideBar
--- frontend/src/vue/src/components/SideBar.vue
+++ frontend/src/vue/src/components/SideBar.vue
@@ -4,78 +4,27 @@
       <li class="header">
         NEWS
       </li>
-      <li
-        @mouseover="mouseOver"
-        class="news-item"
+      <newsitem
         v-for="news in newsList"
         v-bind:news="news"
         v-bind:key="news.id"
-        >
-        <p class="title">
-          {{ news.title }}
-        </p>
-        <p class="text">
-          {{ news.text }}
-        </p>
-        </li>
+        v-bind:toggleActive="toggleActive"
+      >
+      </newsitem>
     </ul>
   </div>
 </template>
 
 <script>
+import SideBarNewsItem from './SideBarNewsItem'
+
 export default {
   name: 'sidebar',
-  props: ['newsList'],
-  methods: {
-    mouseOver: function() {
-      return
-    }
+  props: ['newsList', 'toggleActive'],
+  components: {
+    'newsitem': SideBarNewsItem
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
-#sidebar {
-  height: 100%;
-  width: 250px;
-  position: absolute;
-  box-shadow: 5px 0px 10px -2px lightgrey;
-}
-
-ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-li {
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  flex-direction: column;
-  border-bottom: 1px solid lightgrey;
-  padding: 10px;
-}
-
-.header {
-  height: 100px;
-}
-
-.news-item {
-  height: 60px;
-
-  p {
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    justify-content: center;
-  }
-
-  .title {
-    font-size: 20px;
-  }
-
-}
-
-</style>
+<style src="../styles/SideBar.scss" lang="scss" scoped></style>
