0f986d66bece2762e7cbcc9113f4af82ca2654ec
Kasper Ramstrom <kasper.ram@gmail.com>
2018-11-06 14:37:20 +0100

    Begun on frontend sidebar #18

--- frontend/src/vue/src/components/App.vue
+++ frontend/src/vue/src/components/App.vue
@@ -1,31 +1,30 @@
 <template>
   <div id="app">
-    <img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <sidebar v-bind:newsList="newsList" ></sidebar>
   </div>
 </template>
 
 <script>
+import SideBar from './SideBar'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newsList: [
+        { id: 0, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
+        { id: 0, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
+        { id: 0, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
+        { id: 0, title: "News title 0", text: "Lorem Ipsum is simply dummy text of the printing"},
+      ]
     }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  components: {
+    'sidebar': SideBar
   }
 }
 </script>
---
+++ frontend/src/vue/src/components/SideBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="sidebar">
+    <ul>
+      <li class="header">
+        NEWS
+      </li>
+      <li
+        class="news-item"
+        v-for="news in newsList"
+        v-bind:news="news"
+        v-bind:key="news.id"
+        >
+        <p class="title">
+          {{ news.title }}
+        </p>
+        <p class="text">
+          {{ news.text }}
+        </p>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  props: ['newsList']
+}
+</script>
+
+<style lang="scss" scoped>
+
+#sidebar {
+  border-right: 2px solid grey;
+  height: 100%;
+  width: 250px;
+  position: absolute;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  border-bottom: 2px solid grey;
+  padding: 10px;
+}
+
+.header {
+  height: 100px;
+}
+
+.news-item {
+  height: 60px;
+
+  p {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+  }
+
+  .title {
+    font-size: 20px;
+  }
+
+}
+
+</style>
