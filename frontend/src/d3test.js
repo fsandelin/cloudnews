044f685556384b72051490daa00b15ff3f8bf872
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-11-06 15:07:12 +0100

    Added zoom and drag #11

--- frontend/src/d3test.js
+++ frontend/src/d3test.js
@@ -1,44 +1,50 @@
-var mapWidth = 210;
-var mapHeight = 500;
-var mapContainer = d3.select("#app").append("div").attr("class", "mapContainer")
-                    .attr("width", mapWidth)
-                    .attr("height", mapHeight);
+var swedenMap = newSwedenMap();
 
-var svg = mapContainer.append("svg")
-                        .attr("class", "map")
-                        .attr("width", mapWidth)
-                        .attr("height", mapHeight);
+d3.select("#app").append(() => swedenMap.node());
 
 
+function newSwedenMap(type) {
+    var typeFileName = type === "municipalities" ? "sweden-municipalities.json" : "sweden-counties.json";
+    var SIZE = 1.1;
+    var RATIO = 2.1;
+    var mapContainerWidth = SIZE * 230;
+    var mapContainerHeight = mapContainerWidth * RATIO;
+    
+    var zoom = d3.zoom()
+        .scaleExtent([0.5, 20])
+        .on("zoom", zoomed);
+    
+    var mapContainer = d3.create("svg").attr("class", "mapContainer")
+                        .attr("width", mapContainerWidth)
+                        .attr("height", mapContainerHeight)
+                        .call(zoom);
+    
+    var map = mapContainer.append("g").attr("class", "map");
+    
+    function zoomed() {
+        map.attr("transform", d3.event.transform);
+    }
+    
+    var projection = d3.geoMercator().scale(SIZE*900).translate([SIZE*(-165),SIZE*1525]);
+    var path = d3.geoPath().projection(projection);
+    
+    d3.json(typeFileName)
+        .then(function(data) {
+            if (typeFileName === "sweden-municipalities.json") {
+                map.selectAll("path")
+                    .data(topojson.feature(data, data.objects.kommuner).features)
+                    .enter().append("path")
+                    .attr("d", path);
+            }
 
-var map = svg.append("g")
-            .attr("id", "zoomable");
+            if (typeFileName === "sweden-counties.json") {
+                map.selectAll("path")
+                    .data(topojson.feature(data, data.objects.SWE_adm1).features)
+                    .enter().append("path")
+                    .attr("d", path);
+            }
+            
+        });
 
-var transformMatrix = [1, 0, 0, 1, 0, 0];
-
-document.getElementById("zoomable").addEventListener("wheel", function(data) {
-    var scale = data.deltaY < 0 ? 1.1 : 0.95;
-
-    for (var i = 0; i < 4; i++) {
-        transformMatrix[i] *= scale;
-      }
-
-    transformMatrix[4] += ((1 - scale) * 1); //mouse center
-    transformMatrix[5] += ((1 - scale) * 1); //mouse center
-
-    var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
-
-    svg.attr("transform",newMatrix);
-});
-
-var projection = d3.geoMercator().scale(900).translate([-170,1530]);;
-var path = d3.geoPath().projection(projection);
-
-d3.json("kommun.json")
-    .then(function(data) {
-        
-        map.selectAll("path")
-            .data(topojson.feature(data, data.objects.kommuner).features)
-            .enter().append("path")
-            .attr("d", path);        
-    });
+    return mapContainer;
+}
