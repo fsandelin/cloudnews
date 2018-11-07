0c8728575deee63a95da427df76a6a15b306d5a5
falapen <filipgolander@gmail.com>
2018-11-07 16:02:14 +0100

    Changed to using socket.io #26

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,39 @@
-const http = require('http');
+
+// Get configuration things
 require('dotenv').config();
+// Import and instantiate an http server
+const server = require('http').createServer();
+// Import socket.io and hook it up to http-server
+const io = require('socket.io')();
+const MicroserviceConnection = require('./src/MicroserviceConnection');
 
-const hostname = process.env.SERVER_HOST;
-const port = process.env.SERVER_PORT;
+const ws_host = process.env.WS_HOST;
+const ws_port = process.env.WS_PORT;
+const mock_host = process.env.MOCK_HOST;
+const mock_port = process.env.MOCK_PORT;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  const responseObject = { Hello: 'World' };
-  res.end(JSON.stringify(responseObject));
+const clients = [];
+
+// Hook up the websocket server to the http server
+io.on('connect', () => {
+  console.log('Someone connected');
+});
+io.on('connection', (socket) => {
+  clients.push(socket);
+  // console.log(client);
+  const ms = new MicroserviceConnection('TT', mock_host, mock_port, {}, (article) => {
+    console.log(`The following article was received: ${article}`);
+  });
+
+  socket.on('disconnect', () => {
+    clients.pop();
+    console.log('Client disconnected');
+    // ms.disconnect();
+  });
+  socket.on('message', (message) => {
+    console.log(`User tried to send a message but this isn\'t a goddamn chat, anyhow, heres what they wrote: ${message}`);
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}/`);
-});
+io.listen(ws_port);
+// const { services } = JSON.parse(Buffer.from(query_params.services, 'base64').toString().trim());
