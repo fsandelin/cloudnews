b873b3ce3f1e1daaf84c6ccb06d8af17bfd5d78f
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-11-07 17:46:00 +0100

    Integrated D3js Sweden map with VueJS #23

--- frontend/src/vue/src/components/Main.vue
+++ frontend/src/vue/src/components/Main.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="main-section">
-    <div id="mapDiv">
-      <svg class="mapContainer">
-          <g class="map">
-          </g>
-        </svg>
-    </div>
+    <svg class="mapContainer">
+        <g class="map">
+          <path class="municipality" v-for="municipality in municipalities" :d=municipality>
+          </path>
+          <path class="county" v-for="county in counties" :d=county>
+          </path>
+        </g>
+      </svg>
   </div>
 </template>
 
@@ -16,63 +18,57 @@ import municipalities from '../assets/sweden-municipalities.json'
 import counties from '../assets/sweden-counties.json'
 
 export default {
-  name: "main-section",
+  name: "mainsection",
+  data () {
+    return {
+      municipalities: [],
+      counties: []
+    }
+  },
   mounted: function() {
-    this.newSwedenMap();
+    const RATIO = 2.1;
+    const width = d3.select("#main-section").node().getBoundingClientRect().width;
+    const height = d3.select("#main-section").node().getBoundingClientRect().height;
+    const SIZE =
+        width * RATIO < height
+          ? width/500
+          : height/500;
+
+    function zoomed() {
+      d3.select(".map").attr("transform", d3.event.transform);
+    }
+
+    const zoom = d3
+      .zoom()
+      .scaleExtent([0.5, 20])
+      .on("zoom", zoomed);
+
+    d3.select(".mapContainer").call(zoom);
+
+    const projection = d3.geoMercator()
+      .scale(SIZE * 900)
+      .translate([SIZE * -255 + width/2, (SIZE) * 1525]);
+
+    const path = d3.geoPath().projection(projection);
+
+    var countyFeatures = topojson.feature(counties, counties.objects.SWE_adm1).features;
+    var municipalityFeatures = topojson.feature(municipalities, municipalities.objects.kommuner).features;
+
+    for (var index in countyFeatures) {
+      var countyFeature = countyFeatures[index];
+      this.addCounty(path(countyFeature));
+    }
+    for (var index in municipalityFeatures) {
+      var municipalityFeature = municipalityFeatures[index];
+      this.addMunicipality(path(municipalityFeature));
+    }
   },
   methods: {
-    newSwedenMap: function(type) {
-
-      const typeFileName =
-        type === "municipalities"
-          ? municipalities
-          : counties;
-      const SIZE = 1.1;
-      const RATIO = 2.1;
-      const mapContainerWidth = SIZE * 230;
-      const mapContainerHeight = mapContainerWidth * RATIO;
-
-      const zoom = d3
-        .zoom()
-        .scaleExtent([0.5, 20])
-        .on("zoom", zoomed);
-
-      const mapContainer = d3
-        .select(".mapContainer")
-        .attr("width", mapContainerWidth)
-        .attr("height", mapContainerHeight)
-        .call(zoom);
-
-      const map = d3.select(".map");
-
-      function zoomed() {
-        map.attr("transform", d3.event.transform);
-      }
-
-      const projection = d3
-        .geoMercator()
-        .scale(SIZE * 900)
-        .translate([SIZE * -165, SIZE * 1525]);
-
-      const path = d3.geoPath().projection(projection);
-
-      if (typeFileName === municipalities) {
-        map
-          .selectAll("path")
-          .data(topojson.feature(typeFileName, typeFileName.objects.kommuner).features)
-          .enter()
-          .append("path")
-          .attr("d", path);
-      }
-
-      if (typeFileName === counties) {
-        map
-          .selectAll("path")
-          .data(topojson.feature(typeFileName, typeFileName.objects.SWE_adm1).features)
-          .enter()
-          .append("path")
-          .attr("d", path);
-      }
+    addMunicipality: function(municipality) {
+      this.municipalities.push(municipality);
+    },
+    addCounty: function(county) {
+      this.counties.push(county)
     }
   }
 }
