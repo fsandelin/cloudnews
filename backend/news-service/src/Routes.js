fbf9bc1aea57c73205dc80828978edff73e107b3
falapen <filipgolander@gmail.com>
2018-11-14 16:08:38 +0100

    Changed so endpoint actually works and doesn't just add a mock-object #51

--- backend/news-service/src/Routes.js
+++ backend/news-service/src/Routes.js
@@ -3,11 +3,11 @@ const db = require('./controllers/DatabaseInterface');
 
 const router = express.Router();
 
-router.get('/timespan', (req, res) => {
+router.post('/new_articles', (req, res) => {
   const { services, startDate, endDate } = req.params;
   // const servicesString = Buffer.from(req.params.services, 'base64').toString();
   // const { servicesArray } = JSON.parse(servicesString.trim());
-  db.pushArticles([{ title: 'Carlsson & Carlsson på taket!' }], (error) => {
+  db.pushArticles(req.body.articles, (error) => {
     if (error) {
       res.status(500).send(`500 internal server error: ${error}`);
     } else {
