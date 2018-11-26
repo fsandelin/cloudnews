8f0723fa68cd9f145a6d7ec48787332db0046542
Kasper Ramström <kasper.ram@gmail.com>
2018-11-26 15:41:28 +0100

    Initial start on date picker component #113

---
+++ frontend/src/components/DatePicker.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="date-picker" class="full-shadow flex-centering">
+    <div class="header flex-centering">
+
+      <div class="year-month flex-centering">
+        <i class="material-icons previous">
+          navigate_next
+        </i>
+        <p>
+          <span>February</span>
+          <span>2019</span>
+        </p>
+        <i class="material-icons next">
+          navigate_next
+        </i>
+      </div>
+    </div>
+
+    <div class="calendar">
+
+    </div>
+
+    <div class="footer">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "datepicker"
+}
+</script>
+
+<style src="../styles/DatePicker.scss" lang="scss" scoped></style>
+<style src="../styles/Commons.scss" lang="scss" scoped></style>
--- frontend/src/components/Drawer.vue
+++ frontend/src/components/Drawer.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="drawer"
-    class="bottom-shadow full-shadow flex-centering">
+    class="bottom-shadow flex-centering">
     <slot></slot>
     <drawerhandle
       v-bind:isOpen="isOpen"
