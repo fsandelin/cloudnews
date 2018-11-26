7346f7853f0eae62d7cc3f1b4929b79f8e3faf58
Kasper Ramström <kasper.ram@gmail.com>
2018-11-26 11:30:21 +0100

    Added url link icon in drawer news item #112

--- frontend/src/components/DrawerNewsItem.vue
+++ frontend/src/components/DrawerNewsItem.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="active-news-item">
-    <p class="title flex-centering">
-      {{ activeNewsItem.title }}
-    </p>
+    <div class="header flex-centering">
+      <p class="title">
+        {{ activeNewsItem.title }}
+      </p>
+      <i class="material-icons"
+        v-on:click="() => activeNewsItem.url ? openLink : null"
+        v-bind:class="{ url: activeNewsItem.url }">
+        {{ activeNewsItem.url ? 'link' : 'link_off' }}
+      </i>
+    </div>
     <p class="flex-centering">
       {{ activeNewsItem.lead }}
-    </p>
-    <p class="flex-centering">
-      {{ activeNewsItem.url }}
     </p>
     <img
       v-bind:src="activeNewsItem.imgurl"
@@ -24,6 +28,15 @@ export default {
     ...mapGetters([
       'activeNewsItem'
     ])
+  },
+  methods: {
+    openLink: function () {
+      if (this.activeNewsItem.url.startsWith('http')) {
+        window.open(this.activeNewsItem.url, '_blank')
+      } else {
+        window.open(`https://${this.activeNewsItem.url}`, '_blank')
+      }
+    }
   }
 }
 </script>
