0642cacb1e4d134c1c64f283769066ff9e293fbc
Kasper Ramström <kasper.ram@gmail.com>
2018-11-26 12:01:20 +0100

    Added back button to drawer news item #111

--- frontend/src/components/DrawerNewsItem.vue
+++ frontend/src/components/DrawerNewsItem.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="active-news-item">
     <div class="header flex-centering">
+      <i class="material-icons back"
+        v-on:click="toggleActive(activeNewsItem)">
+        reply
+      </i>
       <p class="title">
         {{ activeNewsItem.title }}
       </p>
-      <i class="material-icons"
+      <i class="material-icons url"
         v-on:click="() => activeNewsItem.url ? openLink() : null"
-        v-bind:class="{ url: activeNewsItem.url }">
+        v-bind:class="{ exists: activeNewsItem.url }">
         {{ activeNewsItem.url ? 'link' : 'link_off' }}
       </i>
     </div>
@@ -20,7 +24,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'drawernewsitem',
@@ -30,6 +34,9 @@ export default {
     ])
   },
   methods: {
+    ...mapActions([
+      'toggleActive'
+    ]),
     openLink: function () {
       if (this.activeNewsItem.url.startsWith('http')) {
         window.open(this.activeNewsItem.url, '_blank')
