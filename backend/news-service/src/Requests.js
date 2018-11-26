76a9fe4b4c11b751ee7676ba2c2d52cc29da4619
falapen <filipgolander@gmail.com>
2018-11-26 19:16:36 +0100

    Requests are now added correctly. Requests are checked for completion on adding. Request are partially completed if a requestedResource is available. Requests are removed when all requestedResources are sent. Added function to update the completionstatus on all pending requests. Runs mock scrape-function on missing timespans on statusupdate. Minor refactor. #88

--- backend/news-service/src/Requests.js
+++ backend/news-service/src/Requests.js
@@ -1,69 +1,113 @@
+const R = require('request');
 const db = require('./controllers/DatabaseInterface');
+require('dotenv').config();
+
+const { MIDDLEWARE_HOST, MIDDLEWARE_PORT } = process.env;
+const MIDDLEWARE_ROUTE = '/complete_request';
+const COMPLETE_REQUEST_URL = `http://${MIDDLEWARE_HOST}:${MIDDLEWARE_PORT}${MIDDLEWARE_ROUTE}`;
+
 
 const requests = {};
 
+
+// Should do some api calls to scrape missing timespans for a given service
 function scrapeRequestedResources(neededTimespans) {
   console.log(`We need to scrape for ${neededTimespans.length} missing timespans`);
   console.log(neededTimespans);
+  // Todo: Send some API call to scraper
 }
 
-function completeRequest(request) {
-  console.log('Should get articles from db and send to middleware');
-  // Has Sent some stuffs
-  delete requests[request.requestId];
+
+// Send all requested resources that are complete to the middleware.
+function sendCompletedResources(request) {
+  const { requestId } = request;
+  request.requestedResources.forEach((requestedResource) => {
+    if (requestedResource.sent || !requestedResource.completed) {
+      return;
+    }
+    const { service, from, until } = requestedResource;
+    db.getArticles(service, from, until, (articles) => {
+      const body = { service, requestId, articles };
+      const options = {
+        url: COMPLETE_REQUEST_URL,
+        body,
+        json: true,
+      };
+      R.post(options, (error, response) => {
+        if (response.statusCode === 200) {
+          requestedResource.sent = true;
+          request.incompleteResources -= 1;
+          console.log(`Has successfully sent timespan for ${service}`);
+        }
+        if (request.incompleteResources === 0) {
+          delete requests[request.requestId];
+        }
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          console.log('Got an error when trying to send requested resource');
+          console.log(error);
+        }
+      });
+    });
+  });
 }
 
-function checkRequestCompletion(request) {
-  const { requestId, requestedResources } = request;
+// Check all requested resources for completion and scrape if missing. Then send all unsent, completed resources.
+function updateRequestCompletion(request) {
+  const { requestedResources } = request;
   let neededTimespans = [];
   let uncheckedResources = requestedResources.length;
-  requestedResources.forEach((requestedResource, index) => {
+  requestedResources.forEach((requestedResource) => {
     if (requestedResource.completed) {
-      console.log('requestedResource completed');
       uncheckedResources -= 1;
       return;
     }
-    db.checkCompletion(requestedResource, (timespans) => {
-      console.log(requestedResource);
+    db.getMissingTimespans(requestedResource, (timespans) => {
       if (timespans.length === 0) {
-        requestedResources[index].completed = true;
+        requestedResource.completed = true;
       } else {
         neededTimespans = neededTimespans.concat(timespans);
       }
       uncheckedResources -= 1;
       if (uncheckedResources === 0) {
-        console.log('Has checked all resources');
         if (neededTimespans.length === 0) {
-          requests[requestId].completed = true;
-          completeRequest(requests[requestId]);
-          console.log(`${requestId} is now marked as completed`);
+          request.completed = true;
         } else {
           scrapeRequestedResources(neededTimespans);
         }
+        sendCompletedResources(request);
       }
     });
   });
 }
 
+// Add a request to the requests-object and check the request for completion.
 function addRequest(requestId, requestedResources) {
+  setInterval(() => {
+    console.log(requests);
+  }, 5000);
+  requestedResources.forEach((requestedResource) => {
+    requestedResource.completed = false;
+    requestedResource.sent = false;
+  });
   requests[requestId] = {
     requestId,
     requestedResources,
-    completed: false,
+    incompleteResources: requestedResources.length,
   };
-  console.log(requests);
-  checkRequestCompletion(requests[requestId]);
+  updateRequestCompletion(requests[requestId]);
 }
 
+// Check all requests for completion.
 function checkRequestsCompletion() {
   const keys = Object.keys(requests);
   for (let i = 0; i < keys.length; i += 1) {
-    checkRequestCompletion(requests[keys[i]]);
+    updateRequestCompletion(requests[keys[i]]);
   }
 }
 
 module.exports = {
   addRequest,
+  updateRequestCompletion,
   checkRequestsCompletion,
   requests,
 };
