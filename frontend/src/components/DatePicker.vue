c903bfab722f63606838cc1a0d7d9b144e1ce478
Kasper Ramström <kasper.ram@gmail.com>
2018-11-27 14:45:22 +0100

    calculate days to fill in current view on date picker calendar #113

--- frontend/src/components/DatePicker.vue
+++ frontend/src/components/DatePicker.vue
@@ -8,14 +8,12 @@
         <i class="material-icons previous">navigate_next</i>
         <p class="flex-col">
           <span class="year">{{ currentYear }}</span>
-          <span class="month">{{ currentMonth }}</span>
+          <span class="month">{{ numToMonth(currentMonth) }}</span>
         </p>
         <i class="material-icons next">navigate_next</i>
       </div>
 
-      <i class="material-icons">
-        cancel
-      </i>
+      <i class="material-icons">cancel</i>
 
     </div>
 
@@ -63,89 +61,106 @@
 </template>
 
 <script>
-
-const JANUARY =  'January';
-const FEBRUARY =  'February';
-const MARCH =  'March';
-const APRIL =  'April';
-const MAY =  'May';
-const JUNE =  'June';
-const JULY =  'July';
-const AUGUST =  'August';
-const SEPTEMBER =  'September';
-const OCTOBER =  'October';
-const NOVEMBER =  'November';
-const DECEMBER =  'December';
-
-const MONDAY =  'Monday';
-const TUESDAY =  'Tuesday';
-const WEDNESDAY =  'Wednesday';
-const THURSDAY =  'Thursday';
-const FRIDAY =  'Friday';
-const SATURDAY =  'Saturday';
-const SUNDAY =  'Sunday';
+import { months as m, weekDays as wd } from '../store/constants';
 
 const tmp_dates = [
-  { year: 2019, month: JANUARY, day: 29 },
-  { year: 2019, month: JANUARY, day: 30 },
-  { year: 2019, month: JANUARY, day: 31 },
-  { year: 2019, month: FEBRUARY, day: 1 },
-  { year: 2019, month: FEBRUARY, day: 2 },
-  { year: 2019, month: FEBRUARY, day: 3 },
-  { year: 2019, month: FEBRUARY, day: 4 },
-  { year: 2019, month: FEBRUARY, day: 5 },
-  { year: 2019, month: FEBRUARY, day: 6 },
-  { year: 2019, month: FEBRUARY, day: 7 },
-  { year: 2019, month: FEBRUARY, day: 8 },
-  { year: 2019, month: FEBRUARY, day: 9 },
-  { year: 2019, month: FEBRUARY, day: 10 },
-  { year: 2019, month: FEBRUARY, day: 11 },
-  { year: 2019, month: FEBRUARY, day: 12 },
-  { year: 2019, month: FEBRUARY, day: 13 },
-  { year: 2019, month: FEBRUARY, day: 14 },
-  { year: 2019, month: FEBRUARY, day: 15 },
-  { year: 2019, month: FEBRUARY, day: 16 },
-  { year: 2019, month: FEBRUARY, day: 17 },
-  { year: 2019, month: FEBRUARY, day: 18 },
-  { year: 2019, month: FEBRUARY, day: 19 },
-  { year: 2019, month: FEBRUARY, day: 20 },
-  { year: 2019, month: FEBRUARY, day: 21 },
-  { year: 2019, month: FEBRUARY, day: 22 },
-  { year: 2019, month: FEBRUARY, day: 23 },
-  { year: 2019, month: FEBRUARY, day: 24 },
-  { year: 2019, month: FEBRUARY, day: 25 },
-  { year: 2019, month: FEBRUARY, day: 26 },
-  { year: 2019, month: FEBRUARY, day: 27 },
-  { year: 2019, month: FEBRUARY, day: 28 },
-  { year: 2019, month: MARCH, day: 1 },
-  { year: 2019, month: MARCH, day: 2 },
-  { year: 2019, month: MARCH, day: 3 },
-  { year: 2019, month: MARCH, day: 4 },
-  { year: 2019, month: MARCH, day: 5 },
-  { year: 2019, month: MARCH, day: 6 },
-  { year: 2019, month: MARCH, day: 7 },
-  { year: 2019, month: MARCH, day: 8 },
-  { year: 2019, month: MARCH, day: 9 },
-  { year: 2019, month: MARCH, day: 10 },
-  { year: 2019, month: MARCH, day: 11 }
+  { year: 2019, month: m.JANUARY, day: 29 },
+  { year: 2019, month: m.JANUARY, day: 30 },
+  { year: 2019, month: m.JANUARY, day: 31 },
+  { year: 2019, month: m.FEBRUARY, day: 1 },
+  { year: 2019, month: m.FEBRUARY, day: 2 },
+  { year: 2019, month: m.FEBRUARY, day: 3 },
+  { year: 2019, month: m.FEBRUARY, day: 4 },
+  { year: 2019, month: m.FEBRUARY, day: 5 },
+  { year: 2019, month: m.FEBRUARY, day: 6 },
+  { year: 2019, month: m.FEBRUARY, day: 7 },
+  { year: 2019, month: m.FEBRUARY, day: 8 },
+  { year: 2019, month: m.FEBRUARY, day: 9 },
+  { year: 2019, month: m.FEBRUARY, day: 10 },
+  { year: 2019, month: m.FEBRUARY, day: 11 },
+  { year: 2019, month: m.FEBRUARY, day: 12 },
+  { year: 2019, month: m.FEBRUARY, day: 13 },
+  { year: 2019, month: m.FEBRUARY, day: 14 },
+  { year: 2019, month: m.FEBRUARY, day: 15 },
+  { year: 2019, month: m.FEBRUARY, day: 16 },
+  { year: 2019, month: m.FEBRUARY, day: 17 },
+  { year: 2019, month: m.FEBRUARY, day: 18 },
+  { year: 2019, month: m.FEBRUARY, day: 19 },
+  { year: 2019, month: m.FEBRUARY, day: 20 },
+  { year: 2019, month: m.FEBRUARY, day: 21 },
+  { year: 2019, month: m.FEBRUARY, day: 22 },
+  { year: 2019, month: m.FEBRUARY, day: 23 },
+  { year: 2019, month: m.FEBRUARY, day: 24 },
+  { year: 2019, month: m.FEBRUARY, day: 25 },
+  { year: 2019, month: m.FEBRUARY, day: 26 },
+  { year: 2019, month: m.FEBRUARY, day: 27 },
+  { year: 2019, month: m.FEBRUARY, day: 28 },
+  { year: 2019, month: m.MARCH, day: 1 },
+  { year: 2019, month: m.MARCH, day: 2 },
+  { year: 2019, month: m.MARCH, day: 3 },
+  { year: 2019, month: m.MARCH, day: 4 },
+  { year: 2019, month: m.MARCH, day: 5 },
+  { year: 2019, month: m.MARCH, day: 6 },
+  { year: 2019, month: m.MARCH, day: 7 },
+  { year: 2019, month: m.MARCH, day: 8 },
+  { year: 2019, month: m.MARCH, day: 9 },
+  { year: 2019, month: m.MARCH, day: 10 },
+  { year: 2019, month: m.MARCH, day: 11 }
 ]
 
 export default {
   name: "datepicker",
   data () {
     return {
-      currentYear: 2019,
-      currentMonth: FEBRUARY,
-      weekDays: [ MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY ],
+      currentYear: 2018,
+      currentMonth: 1,
+      weekDays: [ wd.MONDAY, wd.TUESDAY, wd.WEDNESDAY, wd.THURSDAY, wd.FRIDAY, wd.SATURDAY, wd.SUNDAY ],
       weekNumbers: [5, 6, 7, 8, 9, 10],
       dates: [ ...tmp_dates ]
     }
   },
   computed: {
     daysByRow: function () {
+      const dates = this.getDaysToDisplay()
       return this.weekNumbers.map((_, i) => {
-        return this.dates.slice(i*7, i*7+7)
+        return dates.slice(i*7, i*7+7)
       })
+    }
+  },
+  methods: {
+    numToMonth(num) {
+      return m[Object.keys(m)[num]]
+    },
+    getDaysForMonth(year, month) {
+      return new Date(year, month+1, 0).getDate()
+    },
+    getNumArrayBetweenNums(start, end) {
+      let arr = []
+      for (let i = start; i < end; i++) {
+        arr = [ ...arr, i]
+      }
+      return arr
+    },
+    getDaysToDisplay() {
+      const daysForCurrentMonth = this.getDaysForMonth(this.currentYear, this.currentMonth)
+
+      const currentDaysToFill = this.getNumArrayBetweenNums(1, daysForCurrentMonth+1)
+        .map(i => ({ year: this.currentYear, month: this.currentMonth, day: i }))
+
+      const weekDay = new Date(this.currentYear, this.currentMonth, 1).getDay()-1;
+      const numPreviousDaysToFill = weekDay;
+      const daysForPreviousMonth = this.getDaysForMonth(this.currentYear, this.currentMonth-1) // if january --> previous year instead
+
+      const previousDaysToFill = this.getNumArrayBetweenNums(daysForPreviousMonth-numPreviousDaysToFill+1, daysForPreviousMonth+1)
+        .map(i => ({ year: this.currentYear, month: this.currentMonth-1, day: i }))
+
+      const TOTAL_DAYS_TO_SHOW = 42;
+      const numNextDaysToFill = TOTAL_DAYS_TO_SHOW - (daysForCurrentMonth+numPreviousDaysToFill)
+
+      const nextDaysToFill = this.getNumArrayBetweenNums(1, numNextDaysToFill+1)
+        .map(i => ({ year: this.currentYear, month: this.currentMonth+1, day: i }))
+
+      return [ ...previousDaysToFill, ...currentDaysToFill, ...nextDaysToFill ]
     }
   }
 }
