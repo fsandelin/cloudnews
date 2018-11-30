15c061d77c33153f3334c9309dee3376fe18be61
Kasper Ramström <kasper.ram@gmail.com>
2018-11-30 11:45:52 +0100

    removed commented out code #113

--- frontend/src/store/helpers.js
+++ frontend/src/store/helpers.js
@@ -4,9 +4,6 @@ import { months as m, weekDays as wd } from './constants';
 export const cleanString = s => s.trim().toLowerCase()
 
 export const numToMonth = num => m[Object.keys(m)[num]]
-// export const numToDay = num => wd[Object.keys(wd)[num]]
-
-// export const getCurrentMonth = () => new Date().getMonth();
 
 export const getDaysForMonth = (year, month) => new Date(year, month+1, 0).getDate()
 
