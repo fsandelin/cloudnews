4a0a56321d1c859a57f4b712c87b4bca7485c4ea
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-12-05 14:02:28 +0100

    Fixed indentation #137

--- frontend/src/store/helpers.js
+++ frontend/src/store/helpers.js
@@ -7,7 +7,7 @@ Date.prototype.getWeek = function () {
   const firstThursday = target.valueOf();
   target.setMonth(0, 1);
   if (target.getDay() != 4) {
-      target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+    target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
   }
   return 1 + Math.ceil((firstThursday - target) / 604800000);
 }
