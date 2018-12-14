4ec66a39a79d0f2515c7ff569a32d9a420c7dd4b
Kasper Ramström <kasper.ram@gmail.com>
2018-12-14 15:23:06 +0100

    Added & set up gui.test.js file #160

---
+++ frontend/test/unit/store/modules/gui.test.js
@@ -0,0 +1,49 @@
+import store from '../../../../src/store'
+
+describe('gui getters', () => {
+  describe('getters.zoomValue', () => {
+    // TODO: add tests
+  })
+  describe('getters.showDatePicker', () => {
+    // TODO: add tests
+  })
+  describe('getters.showAboutPage', () => {
+    // TODO: add tests
+  })
+})
+
+describe('gui actions', () => {
+  describe('actions.setZoomValue', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.toggleDatePicker', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.toggleAboutPage', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.toggleDrawer', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.toggleActive', () => {
+    // TODO: add tests
+  })
+})
+
+describe('gui mutations', () => {
+  describe('mutations.setZoomValue', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.toggleDatePicker', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.toggleAboutPage', () => {
+    // TODO: add tests
+  })
+})
