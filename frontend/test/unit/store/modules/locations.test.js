aa0e832cf176bae3522851afbad377ecda0c604a
Kasper Ramström <kasper.ram@gmail.com>
2018-12-14 15:30:07 +0100

    Added & set up locations.test.js file #160

---
+++ frontend/test/unit/store/modules/locations.test.js
@@ -0,0 +1,119 @@
+import store from '../../../../src/store'
+
+describe('locations getters', () => {
+  describe('getters.countries', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.counties', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.municipalities', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.cities', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.mapCities', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.selectedCounty', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.selectedMunicipality', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.selectedCity', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.countyByName', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.countyByid', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.municipalityByName', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.municipalityById', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.cityByName', () => {
+    // TODO: add tests
+  })
+
+  describe('getters.cityById', () => {
+    // TODO: add tests
+  })
+})
+
+describe('locations actions', () => {
+  describe('actions.selectCounty', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.selectMunicipality', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.selectCity', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.setActiveMapCitiesBasedOnPopulation', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.countyClick', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.municipalityClick', () => {
+    // TODO: add tests
+  })
+
+  describe('actions.cityClick', () => {
+    // TODO: add tests
+  })
+})
+
+describe('locations mutations', () => {
+  describe('mutations.selectCounty', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.selectMunicipality', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.selectCity', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.openDrawer', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.closeDrawer', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.toggleActive', () => {
+    // TODO: add tests
+  })
+
+  describe('mutations.setActiveMapCitiesBasedOnPopulation', () => {
+    // TODO: add tests
+  })
+})
