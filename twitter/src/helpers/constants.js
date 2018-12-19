c516f55ec708de62375c7e8472a980afd126426b
Anton Carlsson <mail@antoncarlsson.se>
2018-12-19 11:16:21 +0100

    Refactored the Twitter service. #204

--- twitter/src/helpers/constants.js
+++ twitter/src/helpers/constants.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
-const { NEWS_SERVICE_HOST, NEWS_SERVICE_PORT } = process.env;
-const newsServiceUrl = `http://${NEWS_SERVICE_HOST}:${NEWS_SERVICE_PORT}/api/live_news`;
+const { newsServiceHost, newsServicePort } = process.env;
+const { twitterConsumerKey, twitterConsumerSecret, twitterAccessTokenKey, twitterAccessTokenSecret } = process.env;
+const newsServiceUrl = `http://${newsServiceHost}:${newsServicePort}/api/live_news`;
 const boundingBoxCoordinatesAroundSweden = '10.5922629,55.1365705,24.1773101,69.0600235';
 
 module.exports = {
   newsServiceUrl,
-  boundingBoxCoordinatesAroundSweden
+  boundingBoxCoordinatesAroundSweden,
+  twitterAccessTokenKey,
+  twitterAccessTokenSecret,
+  twitterConsumerKey,
+  twitterConsumerSecret
 };
