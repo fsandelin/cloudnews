ef29de57c7acec92a7501c6e23cdf57aef56f850
Anton Carlsson <mail@antoncarlsson.se>
2018-12-19 16:04:46 +0100

    Extracted all the deployments and the services into individual files for easier management. #19

--- start-cluster.sh
+++ start-cluster.sh
@@ -6,11 +6,18 @@ eval $(minikube docker-env)
 chmod +x docker-build.sh
 ./docker-build.sh
 
-kubectl apply -f microservices.yaml
-kubectl get services
+kubectl apply -f microservices/nginx-node.yaml
+kubectl apply -f microservices/middleware.yaml
+kubectl apply -f microservices/news-service.yaml
+kubectl apply -f microservices/polisen.yaml
+kubectl apply -f microservices/twitter.yaml
 
 chmod +x microservices/mongodb_service/generate.sh
 ./microservices/mongodb_service/generate.sh
+
+kubectl get services
+
+sleep 10
 
 chmod +x microservices/mongodb_service/configure_repset_auth.sh
 ./microservices/mongodb_service/configure_repset_auth.sh
