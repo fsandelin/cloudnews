9f94890c2fdc41cc0dfe3f2b508f19698f78e560
Anton Carlsson <mail@antoncarlsson.se>
2018-12-19 14:09:54 +0100

    Fixed issues with the Twitter service. #230

--- twitter/src/helpers/constants.js
+++ twitter/src/helpers/constants.js
@@ -1,7 +1,4 @@
-require('dotenv').config();
-const { newsServiceHost, newsServicePort } = process.env;
-const { twitterConsumerKey, twitterConsumerSecret, twitterAccessTokenKey, twitterAccessTokenSecret } = process.env;
-const newsServiceUrl = `http://${newsServiceHost}:${newsServicePort}/api/live_news`;
+const { newsServiceUrl, twitterConsumerKey, twitterConsumerSecret, twitterAccessTokenKey, twitterAccessTokenSecret } = require('./../../env');
 const boundingBoxCoordinatesAroundSweden = '10.5922629,55.1365705,24.1773101,69.0600235';
 
 module.exports = {
