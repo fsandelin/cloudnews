fbd3b023d59730f13110dd2176dedc7c29c71fbd
Anton Carlsson <mail@antoncarlsson.se>
2018-12-19 13:28:45 +0100

    Changed port for middleware from 5001 to 8091. Created a script for running the cluster. #19

--- docker-run.sh
+++
@@ -1,5 +0,0 @@
-docker run --name twitter -d twitter-service:latest
-docker run --name polisen -p 4002:4002 -d polisen-service:latest
-docker run --name news-service -p 3000:3000 -d news-service:latest
-docker run --name middleware -p 3001:3001 -p 5001:5001 -d middleware:latest
-docker run --name nginx-node -p 80:8080 -e "NODE_PORT=5000" -d nginx-node:latest
---
+++ start-cluster.sh
@@ -0,0 +1,11 @@
+minikube start
+eval $(minikube docker-env)
+
+chmod +x docker-build.sh
+./docker-build.sh
+
+kubectl apply -f microservices.yaml
+kubectl get services
+
+kubectl port-forward svc/nginx-node-svc 80:80 &
+kubectl port-forward svc/middleware-svc 5001:5001 &
